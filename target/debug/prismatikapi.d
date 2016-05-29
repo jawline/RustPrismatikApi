cd235358868c0f4ec752e6345d590b776a740eb1
Blake Loring <blake_l@parsed.uk>
2016-05-29 19:37:56 +0100

    Remove target

--- target/debug/prismatikapi.d
+++
@@ -1,5 +0,0 @@
-/Users/pbai002/Dropbox/Project Archives/Hobby/prismatikapi/target/debug/prismatikapi.d: src/lib.rs
-
-/Users/pbai002/Dropbox/Project Archives/Hobby/prismatikapi/target/debug/libprismatikapi.rlib: src/lib.rs
-
-src/lib.rs:
